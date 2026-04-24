872ae3c10ef4e57e59971597dc7cb36cf4da75cc
Zike Zhou <zikez95@outlook.com>
2026-04-23 22:39:07 -0400

    Initial Commit

---
+++ Sought/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sought
+//
+//  Created by Zike Zhou on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sought/SoughtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoughtApp.swift
+//  Sought
+//
+//  Created by Zike Zhou on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct SoughtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
